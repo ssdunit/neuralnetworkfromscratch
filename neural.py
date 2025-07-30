93017b6a5823245928e87c8ef88ed75e16aa2b7c
ssdunit <ssdunit456@gmail.com>
2025-07-30 20:18:26 +0530

    neural.py modified , used numpy library to take dot product of input neurons and weights

--- neural.py
+++ neural.py
@@ -1,15 +1,14 @@
 import torch
 import random as rand
+import numpy as np
 input_neurons = [2,1,3,4]
 weights = torch.rand(5,4)
-bias = rand.sample(range(1,100), 4)
+bias = rand.sample(range(1,100),4)
 counter = 0
 neutron_out = []
 for neuron_weight_tensor_list,bias_n in zip(weights,bias):
     neutron_output = 0
-    for inp_n , weights_n in zip(input_neurons,neuron_weight_tensor_list):
-        neutron_output += inp_n*weights_n
-    neutron_output+=bias[counter]
+    neutron_output+=(np.dot(input_neurons,neuron_weight_tensor_list)+bias[counter])
     counter+=1
     neutron_out.append(neutron_output)
 print(neutron_out)
